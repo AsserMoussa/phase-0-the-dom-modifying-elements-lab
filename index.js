d1f2bf29f0f83cef7c4064ba19c3aaca082555f9
assermoussa <asser.y.moussa@gmail.com>
2023-12-09 16:25:55 -0600

    commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const element = document.createElement("div");
+const ul = document.createElement("ul");
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement("li");
+    li.textContent = (i + 1).toString();
+  }
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "is the champion";
